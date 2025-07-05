265b73d4b201dce2225224750fe1d275ca45e54d
Subash Praveen <subashpraveen@proton.me>
2025-07-05 15:28:14 +0530

    added solutions
    - 210

---
+++ Java/210_count_of_common_characters_in_two_strings.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+
+        List<Character> l1 = new ArrayList<>();
+        for (char ch : s1.toCharArray()) {
+            l1.add(ch);
+        }
+
+        int count = 0;
+        for (char ch : s2.toCharArray()) {
+            if (l1.contains(ch)) {
+                count++;
+                l1.remove((Character) ch);
+            }
+        }
+
+        System.out.println(count);
+        sc.close();
+    }
+}
