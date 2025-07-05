c103a890e98c790b87edf7d2f68580dca2682241
Subash Praveen <subashpraveen@proton.me>
2025-07-05 16:01:47 +0530

    added solution
    - 2666

---
+++ Java/2666_matrix_diagonal_sum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] arr = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += arr[i][i]; 
+            if (i != N - 1 - i) {
+                sum += arr[i][N - 1 - i];
+        }
+
+        System.out.println(sum);
+        sc.close();
+    }
+}
